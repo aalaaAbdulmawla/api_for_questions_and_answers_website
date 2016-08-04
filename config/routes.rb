32dbe020730e9f30e0c2516fff80aad757e2c706
aalaaAbdulmawla <alaa.abdulmawla@gmail.com>
2016-08-04 13:49:56 +0200

    Sets the default api version

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
+require 'api_constraints'
 Rails.application.routes.draw do
   devise_for :users
   namespace :api, defaults: { format: :json }, 
                   constraints: { subdomain: 'api'}, path: '/' do
-
+      scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
+      end
   end
 
 end
